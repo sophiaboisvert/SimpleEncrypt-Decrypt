bd5e06016745c3f4699518d8314e393ebb4b8c42
Sophia Boisvert <55329522+sophiaboisvert@users.noreply.github.com>
2019-09-14 14:29:31 -0500

    Update SimpleEncrypt-Decrypt.cpp

--- SimpleEncrypt-Decrypt.cpp
+++ SimpleEncrypt-Decrypt.cpp
@@ -1,14 +1,19 @@
 //File name: SimpleEncrypt-Decrypt.cpp
 //Author: Sophia Boisvert
 //Description: this program implements a cipher to encrypt or decrypt a message
-//Last changed: 26 October, 2017
+//Last changed: 14 September, 2017
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
+//decrypts a file based on a key file
 void decrypt(ifstream& in_stream, ofstream& out_stream);
+//creates a key file and encypts according to the random file. 
 void encrypt(ifstream& in_stream, ofstream& out_stream);
+
 int main()
 {
  char input;
@@ -16,18 +21,17 @@ int main()
  char outputfile [33];
  ifstream in_file;
  ofstream out_file;
- //print out header
- cout << "Computer Science and Engineering\nCSCE 1030 - Computer Science I\nSophia Boisvert\tsmb0506\tSophiaBoisvert@my.unt.edu" << endl;
  
  do
  {
- cout << "\nWould you like to encrypt or decrypt <E or D>: ";
- cin >> input;
+	cout << "\nWould you like to encrypt or decrypt <E or D>: ";
+ 	cin >> input;
  }while(input != 'E' && input != 'e' && input != 'd'&& input != 'D');
  
  cout << "Enter name of the input file: ";
  cin >> inputfile;
  in_file.open(inputfile);
+	
  if(in_file.fail())
  {
   cout<< "Error opening input file" << endl;
@@ -37,11 +41,13 @@ int main()
  cout << "Enter name of the output file: ";
  cin >> outputfile;
  out_file.open(outputfile);
+	
  if(out_file.fail())
  {
   cout<< "Error opening output file";
   exit(EXIT_FAILURE);
  }
+	
  if(input == 'E' || input == 'e')
   encrypt(in_file, out_file);
  else
@@ -49,21 +55,27 @@ int main()
  
  in_file.close();
  out_file.close();
+	
  return 0;
 }
 
 void decrypt(ifstream& in_stream, ofstream& out_stream)
 {
+	
  ifstream key_file;
+
  char keyfile [33];
+
  cout << "Enter name of the encryption key file: ";
  cin >> keyfile;
  key_file.open(keyfile);
+	
  if(key_file.fail())
  {
   cout<< "Error opening encryption key file";
   exit(EXIT_FAILURE);
  }
+	
  char value;
  int keyValue;
  
@@ -98,8 +110,10 @@ void encrypt(ifstream& in_stream, ofstream& out_stream)
 {
  ofstream key_file;
  char keyfile [33];
+	
  cout << "Enter name of the encryption key file: ";
  cin >> keyfile;
+	
  key_file.open(keyfile);
  if(key_file.fail())
  {
@@ -107,11 +121,14 @@ void encrypt(ifstream& in_stream, ofstream& out_stream)
   exit(EXIT_FAILURE);
  }
  srand(time(NULL));
+	
  char value;
  int keyValue;
+	
  while(!in_stream.eof())
  {
   in_stream >> value;
+	 
   if(value >= 65 && value <= 90)
    {
     keyValue = rand() % 277 + 3;
@@ -119,6 +136,7 @@ void encrypt(ifstream& in_stream, ofstream& out_stream)
     out_stream.put(value);
     key_file << keyValue << " ";
    }
+	 
   else if(value >= 97 && value <= 122)
    {
     keyValue = rand() % 277 + 3;
@@ -126,6 +144,7 @@ void encrypt(ifstream& in_stream, ofstream& out_stream)
     out_stream.put(value);
     key_file << keyValue << " ";
    }
+	 
   else if(value >= 48 && value <= 57)
    {
     keyValue = rand() % 277 + 3;
@@ -133,8 +152,10 @@ void encrypt(ifstream& in_stream, ofstream& out_stream)
     out_stream.put(value);
     key_file << keyValue << " ";
    }
+	 
   else if(ispunct(value))
    out_stream.put(value);
+	 
   else
 	cout << "invalid value of " << value;
  
